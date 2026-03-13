76813bcf483657de95cf6aa5a129f16bfbadc561
Nathan <84826494+FFlap@users.noreply.github.com>
2026-03-12 19:18:44 -0600

    Many Test Cases from outline
    - Made CreateEventActivtityTests, HomeActivityTest, HostedEventsActivityTest, LoginActvityTest, MyWaitlistActivityTest, and ViewEventActivity tests

--- app/src/androidTest/java/com/example/eventlotterysystem/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/eventlotterysystem/LoginActivityTest.java
@@ -1,17 +1,53 @@
 package com.example.eventlotterysystem;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.assertNull;
+
+import android.content.Context;
+import android.os.SystemClock;
+
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.google.firebase.auth.FirebaseAuth;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Tests the functionality of LoginActivity
  */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class LoginActivityTest {
     /**
      * Test to see correct Username and Password connects to the correct user and navigates them to the home screen
      */
     @Test
     public void loginByUsernameTest() {
+        FirebaseAuth.getInstance().signOut();
+        Context context = ApplicationProvider.getApplicationContext();
+        AuthSessionPreference.setRemember(context, false);
 
+        try (ActivityScenario<LoginActivity> ignored = ActivityScenario.launch(LoginActivity.class)) {
+            onView(withId(R.id.loginIdentifierInput))
+                    .perform(replaceText("test"), closeSoftKeyboard());
+            onView(withId(R.id.loginPasswordInput))
+                    .perform(replaceText("test123"), closeSoftKeyboard());
+            onView(withId(R.id.loginButton)).perform(click());
+
+            SystemClock.sleep(4000);
+
+            onView(withId(R.id.myThingsButton)).check(matches(isDisplayed()));
+        }
     }
 
     /**
@@ -19,7 +55,21 @@ public class LoginActivityTest {
      */
     @Test
     public void loginByEmailTest() {
+        FirebaseAuth.getInstance().signOut();
+        Context context = ApplicationProvider.getApplicationContext();
+        AuthSessionPreference.setRemember(context, false);
 
+        try (ActivityScenario<LoginActivity> ignored = ActivityScenario.launch(LoginActivity.class)) {
+            onView(withId(R.id.loginIdentifierInput))
+                    .perform(replaceText("test@gmail.com"), closeSoftKeyboard());
+            onView(withId(R.id.loginPasswordInput))
+                    .perform(replaceText("test123"), closeSoftKeyboard());
+            onView(withId(R.id.loginButton)).perform(click());
+
+            SystemClock.sleep(4000);
+
+            onView(withId(R.id.myThingsButton)).check(matches(isDisplayed()));
+        }
     }
 
     /**
@@ -27,7 +77,22 @@ public class LoginActivityTest {
      */
     @Test
     public void loginWithIncorrectUsernameTest() {
+        FirebaseAuth.getInstance().signOut();
+        Context context = ApplicationProvider.getApplicationContext();
+        AuthSessionPreference.setRemember(context, false);
 
+        try (ActivityScenario<LoginActivity> ignored = ActivityScenario.launch(LoginActivity.class)) {
+            onView(withId(R.id.loginIdentifierInput))
+                    .perform(replaceText("not_a_real_test_user"), closeSoftKeyboard());
+            onView(withId(R.id.loginPasswordInput))
+                    .perform(replaceText("test123"), closeSoftKeyboard());
+            onView(withId(R.id.loginButton)).perform(click());
+
+            SystemClock.sleep(4000);
+
+            onView(withId(R.id.loginButton)).check(matches(isDisplayed()));
+            assertNull(FirebaseAuth.getInstance().getCurrentUser());
+        }
     }
 
     /**
@@ -35,6 +100,21 @@ public class LoginActivityTest {
      */
     @Test
     public void loginWithIncorrectPasswordTest() {
+        FirebaseAuth.getInstance().signOut();
+        Context context = ApplicationProvider.getApplicationContext();
+        AuthSessionPreference.setRemember(context, false);
 
+        try (ActivityScenario<LoginActivity> ignored = ActivityScenario.launch(LoginActivity.class)) {
+            onView(withId(R.id.loginIdentifierInput))
+                    .perform(replaceText("test"), closeSoftKeyboard());
+            onView(withId(R.id.loginPasswordInput))
+                    .perform(replaceText("wrongPassword123"), closeSoftKeyboard());
+            onView(withId(R.id.loginButton)).perform(click());
+
+            SystemClock.sleep(4000);
+
+            onView(withId(R.id.loginButton)).check(matches(isDisplayed()));
+            assertNull(FirebaseAuth.getInstance().getCurrentUser());
+        }
     }
 }
